2b79373d0b332bd4ce7b3b310bdaa934c5592a39
azure <1055380813@qq.com>
2026-04-04 23:17:46 +0800

    refactor: 导出页面

--- lib/src/pages/pages.dart
+++ lib/src/pages/pages.dart
@@ -1,3 +1,5 @@
 export 'home/home_page.dart';
 export 'video_player/video_player_page.dart';
 export 'music_player/music_player_page.dart';
+export 'settings/settings_page.dart';
+export 'not_found_page.dart';
